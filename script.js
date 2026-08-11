05b335564ad6634cb6c59945b8a1434cf176e8c7
María Cecilia <lunama.cecilia@live.com>
2026-08-11 20:52:29 -0300

    estructura metodos arrays

---
+++ script.js
@@ -0,0 +1,54 @@
+console.log("Hola mundo")
+
+//MÉTODOS DE ARRAYS
+const familiares = [
+    { nombre: "María", apellido: "Luna", edad: 45 },
+    { nombre: "Juan", apellido: "González", edad: 17 },
+    { nombre: "Ana", apellido: "Luna", edad: 32 },
+    { nombre: "Pedro", apellido: "González", edad: 12 },
+    { nombre: "Lucía", apellido: "González", edad: 68 },
+    { nombre: "Cecilia", apellido: "Luna", edad: 29 },
+];
+
+// Ejemplo: Imprimir el nombre de TODOS los familiares automáticamente
+familiares.forEach(function (familiar) {
+    // "familiar" representa a CADA objeto del array, uno a la vez
+    // En la primera vuelta, familiar = { nombre: "María", ... }
+    // En la segunda vuelta, familiar = { nombre: "Juan", ... }
+    // Y así sucesivamente...
+    console.log(familiar.edad);
+});
+
+//FILTER
+//   const nuevo = array.filter(function(parámetro) {
+//       return condición;
+//   })
+
+const mayoresDeEdad = familiares.filter(function(familiar) {
+    return familiar.edad >= 18;
+});
+
+console.log(familiares)
+console.log(mayoresDeEdad);
+
+//MAP
+// Ejemplo: Crear un nuevo array con los nombres completos
+const nombresCompletos = familiares.map(function(familiar) {
+    // Por cada familiar, devolvemos un STRING con nombre + apellido
+    return familiar.nombre + " " + familiar.apellido;
+});
+
+console.log(nombresCompletos);
+
+const familiarUno = nombresCompletos[2]
+console.log(familiarUno)
+
+const edadesEn10Anios = familiares.map(function(familiar) {
+    return {
+        nombre: familiar.nombre,
+        edadActual: familiar.edad,
+        edadEn10Anios: familiar.edad + 10,
+    };
+});
+
+console.log(edadesEn10Anios)
